f1e828f7a63ed23083e15b9d2f7d85bb50119489
vivekgargemerson <52440374+vivekgargemerson@users.noreply.github.com>
2019-08-27 11:43:06 +0530

    Update sampleService.java

--- src/sampleService.java
+++ src/sampleService.java
@@ -11,6 +11,12 @@ public class sampleService {
 	@GET
 	@Produces("application/json")
 	public String convertCtoFfromInput(@PathParam("name") String name) {
+		BigDecimal bd2 = new BigDecimal(1.1);
+		int target = -5;
+int num = 3;
+
+target =- num;  // Noncompliant; 
+
 		
  		return "{ name : Hello " + name + "}";
 
